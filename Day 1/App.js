859f40fe7ce3c7a20f895079e2ea9a745e2fe1d5
Navaneeth <navaneeth.boddula32@gmail.com>
2025-01-08 11:10:34 +0530

    Testing

---
+++ Day 1/App.js
@@ -0,0 +1,20 @@
+var b = 20;
+console.log(b);
+
+const c = 30;
+console.log(c);
+
+let a = 10;
+
+console.log(a);
+
+{
+    var q = 10;
+    console.log(q);
+
+    let w = 20;
+    console.log(w);
+
+    let e = 30;
+    console.log(e);
+}
